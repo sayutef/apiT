db2c1184bd2c124e67a0cdbb4d9c8814afbe154a
sayutef <sayueszucha@gmail.com>
2025-03-12 09:38:17 -0600

    asa

--- src/notification/application/repositories/ServiceNotification.go
+++ src/notification/application/repositories/ServiceNotification.go
@@ -10,11 +10,11 @@ type ServiceNotification struct {
     imageService IMessageService
 }
 
-// Asegúrate de que ServiceNotification implemente la interfaz INotification
+
 func (sn *ServiceNotification) Send(message entities.Notification) error {
     log.Println("Sending notification:", message.Message)
 
-    // Usa el servicio imageService para publicar el evento (este es tu método PublishEvent)
+
     err := sn.imageService.PublishEvent("AsignatureCreated", message)
     if err != nil {
         log.Printf("Error sending notification: %v", err)
@@ -24,7 +24,6 @@ func (sn *ServiceNotification) Send(message entities.Notification) error {
     return nil
 }
 
-// Crear una nueva instancia de ServiceNotification
 func NewServiceNotification(imageService IMessageService) *ServiceNotification {
     return &ServiceNotification{imageService: imageService}
 }
